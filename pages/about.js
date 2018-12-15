b83233a945a0ddca6bf75f0a49da70cb83adedaf
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-12-15 08:44:00 +0530

    Added export command to npm scripts

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,13 @@
 import BasicLayout from '../layouts/basic';
+import aboutApi from '../api/about.json';
 
 export default function About() {
   return (
     <BasicLayout>
         <h1>There is nothing special about this About Us page</h1>
+        {
+          aboutApi.map((line, i) => <p key={i}>{line}</p>)
+        }
     </BasicLayout>
   );
 }
