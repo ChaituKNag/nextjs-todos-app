54bf429634e88125cdbd579cb69c242efc4730d8
Naga Chaitanya Konada <nkonada@deloitte.com>
2018-12-15 17:44:10 +0530

    route link highlighting achieved.

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,34 @@
 import Router from 'next/router';
 import { Menu } from 'semantic-ui-react';
+import { Component } from 'react';
 
-const routerPush = (url) => () => Router.push(url);
+class Header extends Component{ 
 
-const Header = () => (
-    <Menu style={{
-        marginTop: '2rem'
-    }}>
-        <Menu.Item onClick={routerPush('/')}>Home</Menu.Item>
-        <Menu.Item onClick={routerPush('/about')}>About Us</Menu.Item>
-    </Menu>
-);
+    state = {
+        activeState: Router.route
+    }
+
+    routerPush = (url) => {
+        Router.push(url);
+        this.setState({
+            activeState: url
+        });
+    }
+
+    render() {
+        const { activeState } = this.state;
+        return (
+            <Menu style={{ marginTop : '2rem' }}>
+                <Menu.Item 
+                    onClick={() => this.routerPush('/')} 
+                    active={ activeState === '/'} 
+                >Home</Menu.Item>
+                <Menu.Item 
+                    onClick={() => this.routerPush('/about')} 
+                    active={ activeState === '/about'}
+                >About Us</Menu.Item>
+            </Menu>
+        )
+    }
+};
 export default Header
